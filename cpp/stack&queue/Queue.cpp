5d2f1d4561ec78e27d251c0e278af99e5d393782
fzhnf <faizahnaf52@gmail.com>
2023-08-20 13:19:36 +0800

    stack & queue cpp

--- cpp/stack&queue/Queue.cpp
+++ cpp/stack&queue/Queue.cpp
@@ -1,15 +1,17 @@
-#include <fstream>
-#include <iostream>
-#include <iterator>
-#include <string>
-#include <vector>
+#include <fstream> // Sertakan file header yang diperlukan untuk input/output file
+#include <iostream> // Sertakan file header yang diperlukan untuk input/output standar
+#include <iterator> // Sertakan file header yang diperlukan untuk operasi iterator
+#include <string>   // Sertakan file header yang diperlukan untuk operasi string
+#include <vector>   // Sertakan file header yang diperlukan untuk operasi vektor
 
 class Node {
 public:
   Node *next;
   std::string value;
 
-  Node() : next(nullptr) {}
+  Node()
+      : next(nullptr) {
+  } // Konstruktor menginisialisasi pointer next menjadi nullptr
 };
 
 class Queue {
@@ -17,66 +19,76 @@ public:
   Node *front;
   Node *rear;
 
-  Queue() : front(nullptr), rear(nullptr) {}
+  Queue()
+      : front(nullptr), rear(nullptr) {
+  } // Konstruktor menginisialisasi pointer front dan rear menjadi nullptr
 
-  bool isEmpty() const { return front == nullptr; }
+  bool isEmpty() const {
+    return front == nullptr;
+  } // Periksa apakah antrian kosong
 
   void enqueue(const std::string &value) {
-    Node *node = new Node();
-    node->value = value;
-    node->next = nullptr;
+    Node *node = new Node(); // Buat instance Node baru
+    node->value = value;     // Assign value ke Node
+    node->next = nullptr;    // Set pointer next menjadi nullptr
     if (isEmpty()) {
-      front = rear = node;
+      front = rear =
+          node; // Jika antrian kosong, set front dan rear menjadi node baru
     } else {
-      rear->next = node;
+      rear->next = node; // Jika tidak, tambahkan node baru di belakang antrian
       rear = node;
     }
   }
 
   std::string dequeue() {
     if (isEmpty()) {
-      std::cout << "Index out of range" << std::endl;
+      std::cout << "Indeks di luar jangkauan"
+                << std::endl; // Cetak pesan kesalahan jika antrian kosong
       return "";
     }
-    Node *node = front;
-    front = front->next;
+    Node *node = front;  // Dapatkan node bagian depan
+    front = front->next; // Pindahkan front ke node berikutnya
     if (front == nullptr) {
-      rear = nullptr;
+      rear =
+          nullptr; // Jika front menjadi nullptr, set rear menjadi nullptr juga
     }
-    std::string value = node->value;
-    delete node;
+    std::string value = node->value; // Dapatkan nilai dari node yang di-dequeue
+    delete node;                     // Hapus node yang di-dequeue
     return value;
   }
 
   void swap(int index1, int index2) {
     if (index1 == index2) {
-      return;
+      return; // Jika indeks sama, tidak perlu ditukar
     }
     if (index1 > index2) {
-      std::swap(index1, index2);
+      std::swap(index1, index2); // Pastikan index1 lebih kecil dari index2
     }
     Node *node1 = front;
     Node *node2 = front;
     for (int i = 0; i < index1 && node1; ++i) {
-      node1 = node1->next;
+      node1 = node1->next; // Pindahkan node1 ke indeks yang diinginkan
     }
     for (int i = 0; i < index2 && node2; ++i) {
-      node2 = node2->next;
+      node2 = node2->next; // Pindahkan node2 ke indeks yang diinginkan
     }
     if (node1 && node2) {
-      std::swap(node1->value, node2->value);
+      std::swap(node1->value,
+                node2->value); // Tukar nilai node pada index1 dan index2
     }
   }
 
   std::string get(int index) {
     Node *node = front;
     for (int i = 0; i < index && node; ++i) {
-      node = node->next;
+      node = node->next; // Pindahkan node ke indeks yang diinginkan
     }
     if (node) {
-      return node->value;
+      return node->value; // Kembalikan nilai pada indeks yang ditentukan
     } else {
-      std::cout << "Index out of range" << std::endl;
+      std::cout
+          << "Indeks di luar jangkauan"
+          << std::endl; // Cetak pesan kesalahan jika indeks di luar jangkauan
       return "";
     }
   }
@@ -84,49 +96,56 @@ public:
 
 Queue csv_to_queue(const std::string &filename) {
   Queue queue;
-  std::ifstream csvfile(filename);
+  std::ifstream csvfile(filename); // Buka file CSV input
   std::string line;
   std::vector<std::string> lines;
 
   while (std::getline(csvfile, line)) {
-    queue.enqueue(line);
+    queue.enqueue(line); // Masukkan setiap baris dari file CSV ke dalam antrian
   }
 
   return queue;
 }
 
 void queue_to_csv(const Queue &queue, const std::string &filename) {
-  std::ofstream csvfile(filename);
+  std::ofstream csvfile(filename); // Buka file CSV output
   Node *current = queue.front;
   while (current) {
-    csvfile << current->value << std::endl;
+    csvfile << current->value
+            << std::endl; // Tulis setiap nilai dari antrian ke dalam file CSV
     current = current->next;
   }
 }
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    std::cout << "Usage: " << argv[0] << " <operation> <args...>" << std::endl;
+    std::cout
+        << "Penggunaan: " << argv[0] << " <operasi> <argumen...>"
+        << std::endl; // Cetak petunjuk penggunaan jika argumen tidak mencukupi
     return 1;
   }
 
-  Queue queue = csv_to_queue("List.csv");
+  Queue queue = csv_to_queue("List.csv"); // Konversi file CSV menjadi antrian
 
   std::string operation = argv[1];
 
   if (operation == "swap" && argc == 4) {
-    queue.swap(std::stoi(argv[2]), std::stoi(argv[3]));
+    queue.swap(std::stoi(argv[2]),
+               std::stoi(argv[3])); // Lakukan operasi tukar pada antrian
   } else if (operation == "enqueue" && argc == 3) {
-    queue.enqueue(argv[2]);
+    queue.enqueue(argv[2]); // Lakukan operasi enqueue pada antrian
   } else if (operation == "dequeue") {
-    queue.dequeue();
+    queue.dequeue(); // Lakukan operasi dequeue pada antrian
   } else if (operation == "get" && argc == 3) {
-    std::cout << queue.get(std::stoi(argv[2])) << std::endl;
+    std::cout
+        << queue.get(std::stoi(argv[2]))
+        << std::endl; // Lakukan operasi get pada antrian dan cetak hasilnya
   } else {
-    std::cout << "Invalid operation" << std::endl;
+    std::cout << "Operasi tidak valid"
+              << std::endl; // Cetak pesan kesalahan untuk operasi tidak valid
   }
 
-  queue_to_csv(queue, "List.csv");
+  queue_to_csv(queue, "List.csv"); // Konversi antrian kembali menjadi file CSV
 
   return 0;
 }
--- cpp/stack&queue/Stack.cpp
+++ cpp/stack&queue/Stack.cpp
@@ -1,15 +1,17 @@
-#include <fstream>
-#include <iostream>
-#include <iterator>
-#include <string>
-#include <vector>
+#include <fstream> // Mengimpor header file yang diperlukan untuk operasi file
+#include <iostream> // Mengimpor header file yang diperlukan untuk operasi input/output standar
+#include <iterator> // Mengimpor header file yang diperlukan untuk operasi iterator
+#include <string> // Mengimpor header file yang diperlukan untuk operasi string
+#include <vector> // Mengimpor header file yang diperlukan untuk operasi vektor
 
 class Node {
 public:
-  Node *next;
-  std::string value;
+  Node *next;        // Pointer ke node berikutnya
+  std::string value; // Nilai yang disimpan dalam node
 
-  Node() : next(nullptr) {}
+  Node()
+      : next(nullptr) {
+  } // Konstruktor menginisialisasi pointer next menjadi nullptr
 };
 
 class Stack {
@@ -19,134 +21,163 @@ private:
     Node *current;
 
   public:
-    StackIterator(Node *start) : current(start) {}
+    StackIterator(Node *start)
+        : current(start) {
+    } // Konstruktor iterator, menginisialisasi current dengan start
 
-    bool operator!=(const StackIterator &other) const {
+    bool operator!=(const StackIterator &other)
+        const { // Operator perbandingan tidak sama (!=) untuk iterator
       return current != other.current;
     }
 
-    StackIterator &operator++() {
-      current = current->next;
+    StackIterator &operator++() { // Operator peningkatan (++) untuk iterator
+      current = current->next;    // Memindahkan iterator ke node berikutnya
       return *this;
     }
 
-    std::string operator*() const { return current->value; }
+    std::string operator*() const {
+      return current->value;
+    } // Operator dereferensi (*) untuk mengembalikan nilai dari current node
   };
 
 public:
-  Node *top;
+  Node *top; // Pointer ke node paling atas pada stack
 
-  Stack() : top(nullptr) {}
+  Stack()
+      : top(nullptr) {
+  } // Konstruktor menginisialisasi pointer top menjadi nullptr
 
-  bool hasPop() const { return top != nullptr; }
+  bool hasPop() const {
+    return top != nullptr;
+  } // Mengembalikan true jika stack memiliki elemen untuk di-pop
 
   void push(const std::string &value) {
-    Node *node = new Node();
-    node->value = value;
-    node->next = top;
-    top = node;
+    Node *node = new Node(); // Membuat instance Node baru
+    node->value = value;     // Menetapkan nilai pada Node
+    node->next = top;        // Mengatur next node baru ke top node lama
+    top = node;              // Mengubah top node menjadi node baru
   }
 
   std::string pop() {
     if (hasPop()) {
-      std::string value = top->value;
-      Node *temp = top;
-      top = top->next;
-      delete temp;
-      return value;
+      std::string value =
+          top->value;   // Mendapatkan nilai dari top node yang akan di-pop
+      Node *temp = top; // Membuat pointer sementara ke top node
+      top = top->next;  // Menggeser top ke node berikutnya
+      delete temp;      // Menghapus node yang di-pop dari memori
+      return value;     // Mengembalikan nilai yang di-pop
     } else {
-      std::cout << "Index out of range" << std::endl;
+      std::cout << "Indeks di luar jangkauan"
+                << std::endl; // Mencetak pesan kesalahan jika stack kosong
       return "";
     }
   }
 
   void swap(int index1, int index2) {
     if (index1 == index2) {
-      return;
+      return; // Tidak perlu tukar jika indeks sama
     }
     if (index1 > index2) {
-      std::swap(index1, index2);
+      std::swap(index1, index2); // Memastikan index1 lebih kecil dari index2
     }
     Node *node1 = top;
     Node *node2 = top;
     for (int i = 0; i < index1 && node1; ++i) {
-      node1 = node1->next;
+      node1 = node1->next; // Memindahkan node1 ke indeks yang diinginkan
     }
     for (int i = 0; i < index2 && node2; ++i) {
-      node2 = node2->next;
+      node2 = node2->next; // Memindahkan node2 ke indeks yang diinginkan
     }
     if (node1 && node2) {
-      std::swap(node1->value, node2->value);
+      std::swap(
+          node1->value,
+          node2->value); // Menukar nilai antara node pada index1 dan index2
     }
   }
 
   std::string get(int index) {
     Node *node = top;
     for (int i = 0; i < index && node; ++i) {
-      node = node->next;
+      node = node->next; // Memindahkan node ke indeks yang diinginkan
     }
     if (node) {
-      return node->value;
+      return node->value; // Mengembalikan nilai pada indeks yang ditentukan
     } else {
-      std::cout << "Index out of range" << std::endl;
+      std::cout << "Indeks di luar jangkauan"
+                << std::endl; // Mencetak pesan kesalahan jika indeks di luar
+                              // jangkauan
       return "";
     }
   }
 
-  StackIterator begin() const { return StackIterator(top); }
+  StackIterator begin() const {
+    return StackIterator(top);
+  } // Memberikan iterator untuk elemen pertama stack
 
-  StackIterator end() const { return StackIterator(nullptr); }
+  StackIterator end() const {
+    return StackIterator(nullptr);
+  } // Memberikan iterator untuk elemen setelah elemen terakhir stack
 };
 
 Stack csv_to_stack(const std::string &filename) {
   Stack stack;
-  std::ifstream csvfile(filename);
+  std::ifstream csvfile(filename); // Membuka file CSV input
   std::string line;
   std::vector<std::string> lines;
 
   while (std::getline(csvfile, line)) {
-    lines.push_back(line);
+    lines.push_back(
+        line); // Menambahkan setiap baris dari file CSV ke dalam vektor
   }
 
   for (auto it = lines.rbegin(); it != lines.rend(); ++it) {
-    stack.push(*it);
+    stack.push(*it); // Menambahkan setiap elemen dari vektor ke dalam stack
+                     // secara terbalik
   }
 
   return stack;
 }
 
 void stack_to_csv(const Stack &stack, const std::string &filename) {
-  std::ofstream csvfile(filename);
+  std::ofstream csvfile(filename); // Membuka file CSV output
   for (const std::string &value : stack) {
-    csvfile << value << std::endl;
+    csvfile << value
+            << std::endl; // Menulis setiap nilai dari stack ke dalam file CSV
   }
 }
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    std::cout << "Usage: " << argv[0] << " <operation> <args...>" << std::endl;
+    std::cout << "Penggunaan: " << argv[0] << " <operasi> <argumen...>"
+              << std::endl; // Mencetak petunjuk penggunaan jika argumen tidak
+                            // mencukupi
     return 1;
   }
 
-  Stack stack = csv_to_stack("List.csv");
+  Stack stack = csv_to_stack("List.csv"); // Mengonversi file CSV menjadi stack
 
   std::string operation = argv[1];
 
   if (operation == "swap" && argc == 4) {
-    stack.swap(std::stoi(argv[2]), std::stoi(argv[3]));
+    stack.swap(std::stoi(argv[2]),
+               std::stoi(argv[3])); // Melakukan operasi tukar pada stack
   } else if (operation == "pop") {
-    stack.pop();
+    stack.pop(); // Melakukan operasi pop pada stack
   } else if (operation == "push" && argc == 3) {
-    stack.push(argv[2]);
+    stack.push(argv[2]); // Melakukan operasi push pada stack
   } else if (operation == "hasPop") {
-    stack.hasPop();
+    stack.hasPop(); // Memeriksa apakah stack memiliki elemen yang dapat di-pop
   } else if (operation == "get" && argc == 3) {
-    std::cout << stack.get(std::stoi(argv[2])) << std::endl;
+    std::cout
+        << stack.get(std::stoi(argv[2]))
+        << std::endl; // Melakukan operasi get pada stack dan mencetak hasilnya
   } else {
-    std::cout << "Invalid operation" << std::endl;
+    std::cout
+        << "Operasi tidak valid"
+        << std::endl; // Mencetak pesan kesalahan untuk operasi tidak valid
   }
 
-  stack_to_csv(stack, "List.csv");
+  stack_to_csv(stack, "List.csv"); // Mengonversi stack kembali menjadi file CSV
 
   return 0;
 }
